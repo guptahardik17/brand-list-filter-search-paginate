6fd99ae29324cb225c1c907b669cb3eb19db8ba9
guptahardik17 <hardik-dockabl@hardik-dockabls-MacBook-Air.local>
2019-09-26 11:23:45 +0530

    change

---
+++ build/precache-manifest.69821eb03d92c2bd8e695e201fff34d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b9d397d76f1acf87dab",
+    "url": "/Job-Listing-with-Filters-and-Pagination/static/js/runtime~main.6b10f9ec.js"
+  },
+  {
+    "revision": "0f2832b38efbd1de49d5",
+    "url": "/Job-Listing-with-Filters-and-Pagination/static/js/main.cb1764ad.chunk.js"
+  },
+  {
+    "revision": "495d3448a1934e2f8450",
+    "url": "/Job-Listing-with-Filters-and-Pagination/static/js/2.9222f468.chunk.js"
+  },
+  {
+    "revision": "0f2832b38efbd1de49d5",
+    "url": "/Job-Listing-with-Filters-and-Pagination/static/css/main.2fed3e5a.chunk.css"
+  },
+  {
+    "revision": "62502ec3fe87fef26d168428cf5210c6",
+    "url": "/Job-Listing-with-Filters-and-Pagination/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Job-Listing-with-Filters-and-Pagination/precache-manifest.69821eb03d92c2bd8e695e201fff34d6.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Job-Listing-with-Filters-and-Pagination/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
